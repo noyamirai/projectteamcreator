63b45c2120fe883a7176d8451bd1eb0ba6635e1d
noyamirai <planet1109@hotmail.com>
2022-02-10 16:00:06 +0100

    added: prettier code

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
-})
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
-})
+});
