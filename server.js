48c8681fc7593c22a91aced1cbea1ff85151f1cb
noyamirai <planet1109@hotmail.com>
2022-02-17 15:53:18 +0100

    added: hbs template engine things

--- server.js
+++ server.js
@@ -2,8 +2,25 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const handlebars = require('express-handlebars');
+
+app.set('view engine', 'hbs');
+
+app.engine('hbs', handlebars.engine({
+  layoutsDir: __dirname + '/views/layouts',
+  partialsDir: __dirname + '/views/partials/',
+  defaultLayout: "default",
+  extname: 'hbs'
+}));
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('main', {layout : "default", post: { author: "Maijla"} });
+});
+
+app.get('*', function(req, res){
+  res.status(404).send('Page not found!');
 });
 
 app.listen(port, () => {
