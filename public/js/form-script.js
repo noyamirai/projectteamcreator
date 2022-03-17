cb9c32b5fe0ce9e88f08a266cdffe8715496f513
noyamirai <planet1109@hotmail.com>
2022-03-17 12:24:45 +0100

    feat(swipe): close form on swipe down

--- public/js/form-script.js
+++ public/js/form-script.js
@@ -1,7 +1,11 @@
+/* eslint-disable quotes */
 const formTriggerBtn = document.querySelector(`.form-trigger`);
 const bodyEl = document.querySelector(`body`);
 const formEl = document.querySelector(`form`);
 const closeFormBtn = document.querySelector(`#close-form`);
+
+let touchstartY = 0;
+let touchendY = 0;
 
 // no href basically
 formTriggerBtn.href = `javascript:void(0)`;
@@ -9,6 +13,13 @@ formTriggerBtn.href = `javascript:void(0)`;
 const toggleClass = () => {
     bodyEl.classList.toggle(`overlay`);
     formEl.classList.toggle(`form-active`);
+};
+
+const handleGesture = () => {
+    if (touchendY > touchstartY) {
+        console.log('swipe down!');
+        toggleClass();
+    }
 };
 
 formTriggerBtn.addEventListener(`click`, () => {
@@ -19,4 +30,14 @@ closeFormBtn.addEventListener(`click`, event => {
     event.preventDefault();
 
     toggleClass();
+});
+
+// BRON: https://stackoverflow.com/a/56663695
+formEl.addEventListener("touchstart", e => {
+    touchstartY = e.changedTouches[0].screenY;
+});
+
+formEl.addEventListener("touchend", e => {
+    touchendY = e.changedTouches[0].screenY;
+    handleGesture();
 });
